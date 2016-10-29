ff298da72a93bcb10f7f3587ed83ebf2b637f004
Marcus <lamwc@vcu.edu>
2016-10-29 17:31:59 -0500

    Completed.

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [
+	{
+		name: "Brandon",
+		photo: "/public/media/brandon.jpg",
+		choices: [5, 5, 5, 5, 5, 5, 5, 5, 5, 3]
+	},
+	{
+		name: "Marcus",
+		photo: "/public/media/marcus.jpeg",
+		choices: [5, 5, 2, 3, 4, 4, 3, 5, 4, 5]
+	},
+	{
+		name: "Tony",
+		photo: "/public/media/tony.jpg",
+		choices: [4, 5, 1, 3, 5, 3, 5, 3, 3, 5]
+	}
+];
+
+module.exports = friendsArray;
